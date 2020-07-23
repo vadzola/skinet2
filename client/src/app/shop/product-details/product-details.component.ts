fbbc07514df048468603eeba3c46bd981901f181
vadzola <vadzola@yahoo.com.hk>
2020-07-24 01:24:22 +0200

    End basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -4,6 +4,7 @@ import { ShopService } from '../shop.service';
 import { error } from 'protractor';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,15 +13,31 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
+
   constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
-    // tslint:disable-next-line: align
-    private bcService: BreadcrumbService) {
+              private bcService: BreadcrumbService, private basketService: BasketService) {
       this.bcService.set('@productDetails', '');
      }
 
   ngOnInit() {
     this.loadProduct();
   }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
 
